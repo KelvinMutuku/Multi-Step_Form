bb88a3d61dd4ebdaae1371cbf55a01dceef1d023
Kelvin Mutuku <mutukuk553@gmail.com>
2025-08-26 22:02:54 +0300

    Updated app.blade.php file

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,3 +1,16 @@
-<div>
-    <!-- Do what you can, with what you have, where you are. - Theodore Roosevelt -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Multi-Step Form</title>
+@vite(['resources/css/app.css', 'resources/js/app.js']) {{-- If using Vite --}}
+@livewireStyles
+</head>
+<body class="bg-gray-100">
+<div class="container mx-auto mt-10">
+{{ $slot ?? '' }}
 </div>
+@livewireScripts
+</body>
+</html>
