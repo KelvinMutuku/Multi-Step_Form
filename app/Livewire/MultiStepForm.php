57f4552dec5a4ec1774e0d123464f3a200a0b1c8
Kelvin Mutuku <mutukuk553@gmail.com>
2025-08-27 17:19:40 +0300

    Updated MultiStepForm.php files

--- app/Livewire/MultiStepForm.php
+++ app/Livewire/MultiStepForm.php
@@ -1,11 +1,70 @@
 <?php
-
 namespace App\Livewire;
-
 use Livewire\Component;
-
+use Illuminate\Support\Facades\DB;
+use App\Models\MultiStepForm as MultiStepFormDB;
 class MultiStepForm extends Component
 {
+    public $currentStep = 1;
+    public $totalSteps = 3;
+    public $name;
+    public $email;
+    public $address;
+    public $city;
+    public $gender;
+    public function nextStep()
+    {
+        $this->validateStep();
+        if($this->currentStep < $this->totalSteps){
+            $this->currentStep++;
+        }
+    }
+    public function previousStep()
+    {
+        $this->currentStep--;
+    }
+    public function validateStep()
+    {
+        if($this->currentStep === 1){
+            $rules = [
+                'name' => 'required|string',
+                'email' => 'required|email',
+            ];
+            $this->validate($rules);
+            $this->saveProgress($this->email, ['name' => $this->name], ['email' => $this->email]);
+            session()->flash('success', 'Step '.$this->currentStep.' saved');
+        }elseif($this->currentStep === 2){
+            $rules = [
+                'address' => 'required|string',
+                'city' => 'required|string',
+            ];
+            $this->validate($rules);
+            $this->saveProgress($this->email, ['address' => $this->address], ['city' => $this->city]);
+            session()->flash('success', 'Step '.$this->currentStep.' saved');
+        }elseif($this->currentStep === 3){
+            $rules = [
+                'gender' => 'required|string',
+            ];
+            $this->validate($rules);
+            $this->saveProgress($this->email, ['gender' => $this->gender]);
+            session()->flash('success', 'Step '.$this->currentStep.' saved');
+        }
+    }
+    public function saveProgress($email, ...$formFields)
+    {
+        $data = [];
+        foreach($formFields as $value){
+            if (is_array($formFields)) {
+                $data = array_merge($data, $value);
+            }
+        }
+        try{
+            MultiStepFormDB::updateOrCreate(['email' => $email], $data);
+        }catch(\Exception $e){
+            \Log::error('Error saving MultiStepForm: ' . $e->getMessage());
+            session()->flash('error', 'There was an error saving your data. Please try again.');
+        }
+    }
     public function render()
     {
         return view('livewire.multi-step-form');
--- app/Models/MultiStepForm.php
+++ app/Models/MultiStepForm.php
@@ -1,10 +1,12 @@
 <?php
-
 namespace App\Models;
-
-use Illuminate\Database\Eloquent\Model;
-
+use MongoDB\Laravel\Eloquent\Model;
 class MultiStepForm extends Model
 {
-    //
+protected $connection = 'mongodb';
+protected $table = 'multi_step_form';
+ 
+protected $fillable = [
+'name', 'email', 'address', 'city', 'gender',
+];
 }
